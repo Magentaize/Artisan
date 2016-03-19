4f7678eaa5c6e39f9b66bea82531ecee8afb0503
拖拉姬 <peoleser@gmail.com>
2016-03-19 23:38:58 +0800

    interface

--- Artisan/Interface/IUserInfo.cs
+++ Artisan/Interface/IUserInfo.cs
@@ -1,13 +1,7 @@
 ﻿namespace Artisan.Interface
 {
-    public interface IUserInfo
+    public interface IUserInfo:IUser
     {
-        string Uid { get; set; }
-        string NickName { get; set; }
-        string HeadPic { get; set; }
-        int Gender { get; set; }
-        IGeo Geo { get; set; }
-        string Intro { get; set; }
         bool IsFollow { get; set; }
         int Works { get; set; }
         int Article { get; set; }
