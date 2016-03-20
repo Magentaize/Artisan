9ec5daa39088241e56b489c6459afe91737958d7
拖拉姬 <peoleser@gmail.com>
2016-03-20 21:26:33 +0800

    ...

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Artisan.Toolkit.Net;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task<Dictionary<string,string> > TestMethod1()
+        {
+          // var result = await HttpWebPost.PostDataToUriAsync("http://artisan.chinacloudapp.cn/signup.json", "nickname=zix&username=rukamihara&password=20132013");
+            //return result;
+        }
+    }
+}
